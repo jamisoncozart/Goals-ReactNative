2a053e5e6e3bf329201a564ed6baad427259896a
Jamison Cozart <jamisoncozart@gmail.com>
2020-05-25 15:27:07 -0700

    Add goalInput, adding new goal functionality, and listing goals

--- App.js
+++ App.js
@@ -1,19 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [goalList, setGoalList] = useState([]);
+
+  function handleGoalInputChange(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+
+  function handleAddingNewGoal() {
+    setGoalList(currentGoals => [...currentGoals, enteredGoal]);
+    setEnteredGoal('');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          onChangeText={handleGoalInputChange}
+          value={enteredGoal} 
+          placeholder='Goal' 
+          style={styles.input} />
+        <Button 
+          title='ADD' 
+          onPress={handleAddingNewGoal}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        {goalList.map((goal, index) => 
+          <View 
+            style={styles.goal} 
+            key={index}>
+              <Text>{goal}</Text>
+          </View>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1, 
+    marginBottom: 5, 
+    padding: 5, 
+    width: '80%'
+  },
+  goal: {
+    padding: 10,
+    backgroundColor: '#eee',
+    marginVertical: 5
+  }
 });
